cdc9fa6557a2a3e732fcbd6f02c6ac933aa8606f
willames <willamessantos@live.com>
2022-05-20 16:25:55 -0300

    add file css

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function renderizaHtml(path, diretorio, res, fs) {
     fs.readFile(`${diretorio}${path}`, (err, html) => {
         res.writeHeader(200, {'Content-Type': 'text/html; charset=utf-8'});
         res.end(html);
-    });  
+    });
 }
 
 const server = http.createServer((req, res) => {
@@ -17,7 +17,13 @@ const server = http.createServer((req, res) => {
         renderizaHtml('/index.html', __dirname, res, fs)
     }else if(result.pathname === '/contato.html'){
         renderizaHtml('/contato.html', __dirname, res, fs)
-    }else {
+    }else if(result.pathname === '/styles/index.css') {
+        fs.readFile(__dirname + '/styles/index.css', (err, css) => {
+            res.writeHeader(200, {'Content-Type': 'text/css; charset=utf-8'});
+            res.end(css);
+        });
+    }
+    else {
         renderizaHtml('/erro.html', __dirname, res, fs)
     }
 }) 
