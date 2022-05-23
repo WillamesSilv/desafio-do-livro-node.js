d5920f790089f24b4fe30fd8babc6478a2bfced9
willames <willamessantos@live.com>
2022-05-23 15:03:37 -0300

    refatorando

--- server.js
+++ server.js
@@ -9,23 +9,20 @@ function renderizaHtml(path, diretorio, res, fs) {
     });
 }
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async function(req, res) {
     res.writeHead(200, { "Content-type":"text/html; charset=uft-8"})
     const result = url.parse(req.url, true)
-    if(result.pathname === '/' || result.pathname === '/index.html'){
+    await (result.pathname === '/') ? renderizaHtml('/index.html', __dirname, res, fs) : 
+    renderizaHtml(result.pathname, __dirname, res, fs)
+    /*if(result.pathname === '/' || result.pathname === '/index.html'){
         // A constante __dirname retorna o diretório raiz da aplicação.
         renderizaHtml('/index.html', __dirname, res, fs)
     }else if(result.pathname === '/contato.html'){
         renderizaHtml('/contato.html', __dirname, res, fs)
-    }else if(result.pathname === '/styles/index.css') {
-        fs.readFile(__dirname + '/styles/index.css', (err, css) => {
-            res.writeHeader(200, {'Content-Type': 'text/css; charset=utf-8'});
-            res.end(css);
-        });
     }
     else {
         renderizaHtml('/erro.html', __dirname, res, fs)
-    }
+    }*/
 }) 
 
 server.listen(3000, () => console.log("Servidor rodando na porta 3000..."))
