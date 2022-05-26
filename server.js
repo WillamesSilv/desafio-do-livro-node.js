dabfd810d599985081882a8ad0b961e0707a2589
willames <willamessantos@live.com>
2022-05-26 10:15:56 -0300

    Melhorando as rotas e as respostas da requisição ao arquivo css

--- server.js
+++ server.js
@@ -9,20 +9,32 @@ function renderizaHtml(path, diretorio, res, fs) {
     });
 }
 
-const server = http.createServer(async function(req, res) {
+function renderizaCss(path, diretorio, res, fs){
+    fs.readFile(`${diretorio}${path}`, (err, css) => {
+        res.writeHeader(200, {'Content-Type': 'text/css; charset=utf-8'});
+        res.end(css);
+    });
+}
+
+const server = http.createServer(function(req, res) {
     res.writeHead(200, { "Content-type":"text/html; charset=uft-8"})
-    const result = url.parse(req.url, true)
-    await (result.pathname === '/') ? renderizaHtml('/index.html', __dirname, res, fs) : 
-    renderizaHtml(result.pathname, __dirname, res, fs)
-    /*if(result.pathname === '/' || result.pathname === '/index.html'){
+    const {pathname} = url.parse(req.url, true)
+    
+    if(pathname === '/' || pathname.slice(-4) === 'html'){
         // A constante __dirname retorna o diretório raiz da aplicação.
-        renderizaHtml('/index.html', __dirname, res, fs)
-    }else if(result.pathname === '/contato.html'){
-        renderizaHtml('/contato.html', __dirname, res, fs)
+        let path = pathname === '/' ? '/index.html' : pathname.slice(pathname.indexOf('/'), pathname.length)
+        renderizaHtml(path, __dirname, res, fs)
+
+    }else if (pathname.slice(-3) === 'css') {
+        
+        renderizaCss(pathname.slice(pathname.indexOf('/'), pathname.length), __dirname, res, fs)
+
     }
     else {
+
         renderizaHtml('/erro.html', __dirname, res, fs)
-    }*/
+
+    }
 }) 
 
 server.listen(3000, () => console.log("Servidor rodando na porta 3000..."))
